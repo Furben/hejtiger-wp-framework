bae7536d9afd0847dd1ea950fac6e54cc269aaf4
Jesper T. Rasmussen <jesper@hejtiger.dk>
2019-01-16 07:07:18 +0100

    Updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var uglify = require('gulp-uglify');
 var cache = require('gulp-cache');
+var imagemin = require('gulp-imagemin');
 
 
 //development
@@ -16,11 +18,15 @@ gulp.task('sass', function(){
     // Initializes sourcemaps
     .pipe(sourcemaps.init())
     .pipe(sass({
-      errLogToConsole: true
+      errLogToConsole: true,
+      outputStyle: 'expanded'
       })) // Converts Sass to CSS with gulp-sass
+    .on ('error' , console.error.bind(console))
     .pipe(sourcemaps.write())
     .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('dist'))
+    .pipe(sourcemaps.write( './') )
     .pipe(browserSync.stream());
 });
 
@@ -31,11 +37,25 @@ gulp.task('scripts', function() {
   .pipe(gulp.dest('dist/js'));
 });
 
+
+gulp.task('images', function() {
+  return gulp.src('assets/images/**')
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins: [{removeViewBox: true}]})
+]))
+  .pipe(gulp.dest('dist/images/'));
+});
+
+
 gulp.task('serve',  gulp.series('sass', function() {
    browserSync.init({
         // You can tell browserSync to use this directory and serve it as a mini-server
         // Change proxy to url
-        proxy: 'https://hejtiger.dev.cc',
+        proxy: 'https://hejtiger-framework.dev.cc',
+        browser: ['firefox'],
         injectChanges: true,
         https: true
         // If you are already serving your website locally using something like apache
@@ -45,20 +65,22 @@ gulp.task('serve',  gulp.series('sass', function() {
  }));
 
 var autoprefixerOptions = {
-  browsers: ['last 2 versions']
+  browsers: ['last 2 versions'],
+  cascade: false
 };
 
 // Watch folder for changes
 gulp.task('watch', function (){
   gulp.watch('assets/scss/*.scss', gulp.series('sass'));
   gulp.watch('assets/js/*.js', gulp.series('scripts'));
+  gulp.watch('assets/images/**', gulp.series('images'));
   gulp.watch('*.php').on('change', browserSync.reload);
 });
 
 // Basic Gulp task syntax
 
 gulp.task('default',
-  gulp.series('scripts', 'sass', gulp.parallel('serve', 'watch'))
+  gulp.series('images', 'scripts', 'sass', gulp.parallel('serve', 'watch'))
   );
 
 gulp.task('clean:dist', function() {
